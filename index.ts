aeb6d84935c3f6db87695fe738bfb8903a21c380
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2025-04-16 17:52:40 +0700

    Process all outputs

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import tools_config from "./tools_config";
 import OpenAI from "openai";
 import prompt from "./prompt";
 import processFunction from "./tools";
-import {ResponseFunctionToolCall, ResponseInputItem} from "openai/resources/responses/responses";
+import {ResponseInput} from "openai/resources/responses/responses";
 import {ResponsesModel} from "openai/resources";
 
 const MODEL: ResponsesModel = "gpt-4.1-mini-2025-04-14"
@@ -16,6 +16,8 @@ const getUserInput = async (): Promise<string> => {
             terminal: true,
         })
 
+        rl.prompt()
+
         // Listen for input and build the string
         rl.on('line', (input: string) => {
             rl.close()
@@ -24,96 +26,79 @@ const getUserInput = async (): Promise<string> => {
     });
 };
 
+const request = async (client: OpenAI, input: OpenAI.Responses.ResponseInput, prev_id: string | null) => client.responses.create({
+    model: MODEL,
+    previous_response_id: prev_id,
+    input,
+    text: {
+        format: {
+            type: "text",
+        }
+    },
+    tools: tools_config,
+    temperature: 0.5,
+    max_output_tokens: 1200,
+    top_p: 1,
+    store: true
+})
+
 async function main() {
     const client = new OpenAI({apiKey: process.env.OPENAI_APIKEY})
-
+    // always send the previous response back to the API - even though it's only required for function calling
+    // keeps the logic simple and consistent
+    // so, previous_response_id refers to the one BEFORE the last response
+    // prev_ids[0] is the oldest
+    const prev_ids: [string | null, string | null] = [null, null]
     const firstInput = await getUserInput()
 
-    const firstResponse = await client.responses.create({
-        model: MODEL,
-        input: [...prompt, {role: 'user', content: firstInput}],
-        text: {
-            format: {
-                type: "text"
-            }
-        },
-        reasoning: {},
-        tools: tools_config,
-        temperature: 0.5,
-        max_output_tokens: 1200,
-        top_p: 1,
-        store: true,
-    })
+    // currentInput will be used twice. first, as an input (obviously)
+    // and as the start of inputCache
+    let inputCache: ResponseInput = [...prompt]
+    // as an array because it *might* be empty (when the response only contains function calls for example)
+    // simpler syntax with the spread operator
+    let currentInput: ResponseInput = [{role: 'user', content: firstInput}]
+    // gosh this 'all-new' response API is so trash
 
-    // function calls are not considered
-    let prevID: string | null = null
-    let cachedInput: OpenAI.Responses.ResponseInput = []
-    let currentResponse = firstResponse
 
-    while (true) {
-        //TODO: add a for loop, handle all responses
-        const newInput: OpenAI.Responses.ResponseInput = []
+    // ts warning duh
+    let always = true
+    while (always) {
+        const response = await request(client, [...inputCache, ...currentInput], prev_ids[0])
+        prev_ids[0] = prev_ids[1]
+        prev_ids[1] = response.id
+        inputCache = []
 
-        switch (currentResponse.output[0].type) {
-            case "message": {
-                console.log(`Assistant: ${currentResponse.output_text}`)
-                // only message IDs are considered
-                // because we need to include the previous input for function calling
-                // and putting its ID means it's a duplicate. it doesn't like it.
-                prevID = currentResponse.id
-                cachedInput = []
+        inputCache.push(...currentInput)
+        currentInput = []
 
-                const userMessage = await getUserInput()
-                const newMessage: ResponseInputItem = {role: 'user', content: userMessage}
-                newInput.push(newMessage)
-                cachedInput.push(newMessage)
-                break
-            }
-            case "function_call": {
-                console.log(`${currentResponse.output[0].name}(${currentResponse.output[0].arguments})`)
-                // if prev_id does not exist, that means this is the first message
-                // we have to reinclude the system prompt
-                // and the user message
-                if (!prevID) {
-                    newInput.push(...prompt)
-                    newInput.push({role: 'user', content: firstInput})
+        for (const [index, output] of response.output.entries()) {
+            switch (output.type) {
+                case "message": {
+                    console.log(`Assistant: ${output.content.map(c => c.type == 'output_text' ? c.text : c.refusal).join("\n")}`)
+                    inputCache.push(output)
+
+                    if (index != response.output.length - 1) break
+
+                    const userMessage = await getUserInput()
+                    currentInput.push({role: "user", content: userMessage})
+                    break
                 }
+                case "function_call": {
+                    console.log(`${output.name}(${output.arguments})`)
+                    inputCache.push(output)
 
-                newInput.push(...cachedInput)
+                    const result = await processFunction(output)
+                    console.log(result)
 
-                const latestResponse = currentResponse.output[0] as ResponseFunctionToolCall
-                newInput.push(latestResponse)
-                cachedInput.push(latestResponse)
-                const result = await processFunction(latestResponse)
-                console.log(result)
-
-                const caller: ResponseInputItem = {
-                    type: 'function_call_output',
-                    call_id: latestResponse.call_id,
-                    output: result,
+                    currentInput.push({
+                        type: 'function_call_output',
+                        call_id: output.call_id,
+                        output: result
+                    })
+                    break
                 }
-                newInput.push(caller)
-                cachedInput.push(caller)
-                break
             }
         }
-
-        currentResponse = await client.responses.create({
-            model: MODEL,
-            previous_response_id: prevID,
-            input: newInput,
-            text: {
-                format: {
-                    type: "text"
-                }
-            },
-            reasoning: {},
-            tools: tools_config,
-            temperature: 0.5,
-            max_output_tokens: 1200,
-            top_p: 1,
-            store: true,
-        })
     }
 }
 
