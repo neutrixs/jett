8e9f2ab4bd772ba16318e4cb725b765074069ed7
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2025-04-18 15:20:15 +0700

    replace function

--- tools/tree_manager.ts
+++ tools/tree_manager.ts
@@ -16,82 +16,89 @@ interface ExportResult {
     reason?: string
 }
 
-interface TraverseResult {
+interface SimpleResult {
     success: boolean
     reason?: string
 }
 
 export default class TreeManager {
     private tree: SerializedAXNode
-    private traversal: number[]
+    private currentTraversal: SerializedAXNode[]
 
     public constructor(node: SerializedAXNode) {
-        this.traversal = []
         if (!node.children) {
             throw new Error("Empty nodes. Try again after the page loads.")
         } else {
             this.tree = node
+            this.currentTraversal = node.children
         }
     }
 
-    public traverse(id: string): TraverseResult {
-        const split = id.split(".")
-        const requestedTraversal = split.map(c => parseInt(c))
-
+    private traverse(ids: number[]): SimpleResult {
+        // will always exist
         let current = this.tree.children as SerializedAXNode[]
-        let currentTraversal: number[] = []
-        for (const index of requestedTraversal) {
-            let element = current[index]
-            currentTraversal.push(index)
 
-            if (!element) return {
-                success: false,
-                reason: `ID ${currentTraversal.join('.')} does not exist`
+        for (const id of ids) {
+            const element = current[id]
+            if (!element) {
+                return {
+                    success: false,
+                    reason: `ID ${ids.join('.')} does not exist`
+                }
             }
-
-            if (!element.children) return {
-                success: false,
-                reason: `Cannot traverse to ${currentTraversal.join('.')}. It does not have children`
+            if (!element.children) {
+                return {
+                    success: false,
+                    reason: `Cannot traverse. ID ${ids.join('.')} has no children`
+                }
             }
-
             current = element.children
         }
 
-        this.traversal = requestedTraversal
+        this.currentTraversal = current
 
         return {
             success: true
         }
     }
 
-    public dump(chunk: number): ExportResult {
-        // this will always exist
-        // because it's checked already in constructor
-        let current = this.tree.children as SerializedAXNode[]
-        let currentTraversal: number[] = []
-
-        for (const index of this.traversal) {
-            let element = current[index]
-            currentTraversal.push(index)
-
-            // no need to check if element or its children exist
-            // because whenever traverse is called
-            // it will be checked
-            current = element.children as SerializedAXNode[]
+    private dumpAll(id: string): ExportResult {
+        let ids: number []
+        if (!id) {
+            ids = []
+        } else {
+            ids = id.split('.').map(c => parseInt(c))
+            if(ids.some(c => isNaN(c))) return {
+                success: false,
+                reason: "Invalid ID. ID must be numbers separated by dots"
+            }
         }
 
-        const result: ExportResult["content"] = current.map((e, i) => ({
-            role: e.role,
-            name: e.name || '',
-            hasChildren: "children" in e,
-            id: [...this.traversal, i].join('.'),
-        }))
+        const result = this.traverse(ids)
+        if (!result.success) return result
 
-        const start = chunk * CHUNK_SIZE
+        const content: ExportResult["content"] = this.currentTraversal.map((c, i) => ({
+            role: c.role,
+            name: c.name || '',
+            hasChildren: !!c.children,
+            id: `${ids.join('.')}.${i}`
+        }))
 
         return {
             success: true,
-            content: result.slice(start, start + CHUNK_SIZE),
+            content
+        }
+    }
+
+    public dump(id: string, chunk: number): ExportResult {
+        const result = this.dumpAll(id)
+        if (!result.success || !result.content) return result
+
+        const start = chunk * CHUNK_SIZE
+        const end = start + CHUNK_SIZE
+        return {
+            success: true,
+            content: result.content.slice(start, end)
         }
     }
 }
