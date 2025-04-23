aa9069ab4ad0e7312ec5ba7fd38e69252e15edf5
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2025-04-23 12:23:46 +0700

    simplify tool param

--- tools/index.ts
+++ tools/index.ts
@@ -1,13 +1,8 @@
 import OpenAI from "openai";
 import db, {DbParam} from "./database";
-import browser, {
-    BrowserEvaluateParam, BrowserOpenParam,
-    BrowserOpenURLParam,
-    ClickParam,
-    DumpParam
-} from "./browser";
+import browser, {BrowserArgs} from "./browser";
 
-export default async function processFunction(call:  OpenAI.Responses.ResponseFunctionToolCall) {
+export default async function processFunction(call: OpenAI.Responses.ResponseFunctionToolCall) {
     let output: string
     try {
         const args = JSON.parse(call.arguments)
@@ -22,37 +17,9 @@ export default async function processFunction(call:  OpenAI.Responses.ResponseFu
             output = JSON.stringify(db.action(args))
             break
         }
-        case 'browser_open': {
-            const args: BrowserOpenParam = JSON.parse(call.arguments)
-            output = JSON.stringify(await browser.open(args.headless))
-            break
-        }
-        case 'browser_close': {
-            output = JSON.stringify(await browser.close())
-            break
-        }
-        case 'browser_open_url': {
-            const args: BrowserOpenURLParam = JSON.parse(call.arguments)
-            output = JSON.stringify(await browser.openURL(args.url))
-            break
-        }
-        case 'browser_evaluate': {
-            const args: BrowserEvaluateParam = JSON.parse(call.arguments)
-            output = JSON.stringify(await browser.evaluate(args.command))
-            break
-        }
-        case 'screen_reader_get_snapshot': {
-            output = JSON.stringify(await browser.get_snapshot())
-            break
-        }
-        case 'screen_reader_dump': {
-            const args: DumpParam = JSON.parse(call.arguments)
-            output = JSON.stringify(browser.dump(args.id, args.chunk))
-            break
-        }
-        case 'screen_reader_click': {
-            const args: ClickParam = JSON.parse(call.arguments)
-            output = JSON.stringify(await browser.click(args.id))
+        case 'browser': {
+            const args: BrowserArgs = JSON.parse(call.arguments)
+            output = JSON.stringify(await browser.call(args))
             break
         }
         default: {
